fb4457374bd9c84613ea75f1c261c09e43960a2f
julianajarm <julianajarm@gmail.com>
2019-04-08 21:23:58 +0300

    commented code deleted

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 "use strict";
 
-//ES 5
 var GOODS = [
     {title: 'Shirt', price: '49.99$'},
     {title: 'Potato', price: '0.99$'},
@@ -24,28 +23,3 @@ function renderGoodsList(list) {
 }
 
 renderGoodsList(GOODS);
-
-
-// ES 6
-// const GOODS = [
-//     {title: 'Shirt', price: '49.99$'},
-//     {title: 'Potato', price: '0.99$'},
-//     {title: 'Hat', price: '9.99$'},
-//     {title: 'Milk', price: '0.99$'},
-//     {title: 'Trousers', price: '29.99$'},
-//     {title: 'Shoes', price: '30.99$'},
-// ];
-//
-// const renderGoodsItem = (title = 'good', price = '0.00$') => `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-//
-// const renderGoodsList = list => {
-//     const goodsList = list.map (item => renderGoodsItem(item.title, item.price));
-//
-//     document.querySelector('.goods-list').innerHTML = goodsList.join('');
-// };
-//
-// renderGoodsList(GOODS);
-//
-
-
-
